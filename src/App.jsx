d6cf3626f3336b472ed81a592846a576c7ef424e
Ursula Ngalubenge <ursulangalubenge2021@gmail.com>
2025-07-03 11:01:33 +0100

    Feat: Adding path to the Navigation Links in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,9 @@ import LogIn from "./components/auth/LogIn.jsx";
 import ErrorPage from "./components/error-page.jsx";
 import ForgotPassword from "./components/auth/ForgotPassword.jsx";
 import Home from "./Pages/home.jsx";
+import Den from "./Pages/den.jsx";
+import Notifications from "./Pages/notifications.jsx"
+import Settings from "./Pages/settings.jsx"
 import Profil from "./Pages/profil.jsx";
 
 // Define the routes for the application
@@ -16,16 +19,32 @@ const router = createBrowserRouter([
     element: <LogIn />,
     errorElement: <ErrorPage />, // ici la page qui s'affichera en cas d'erreur (route non trouvée, etc)
   },
-  { path: "/SignUp", element: <SignUp />, 
+  {
+    path: "/SignUp", element: <SignUp />,
     errorElement: <ErrorPage /> // ici la page qui s'affichera en cas d'erreur (route non trouvée, etc)
   },
-  { path: "/ForgotPassword", element: <ForgotPassword />,
+  {
+    path: "/ForgotPassword", element: <ForgotPassword />,
     errorElement: <ErrorPage /> // ici la page qui s'affichera en cas d'erreur (route non trouvée, etc)
   },
-  { path: "/home", element: <Home /> 
+  {
+    path: "/home", element: <Home />
     , errorElement: <ErrorPage /> // ici la page qui s'affichera en cas d'erreur (route non trouvée, etc)
   },
-  { path: "/profil", element: <Profil />,
+  {
+    path: "/den", element: <Den />,
+    errorElement: <ErrorPage />
+  },
+    {
+    path: "/notifications", element: <Notifications />,
+    errorElement: <ErrorPage />
+  },
+    {
+    path: "/settings", element: <Settings />,
+    errorElement: <ErrorPage />
+  },
+  {
+    path: "/profil", element: <Profil />,
     errorElement: <ErrorPage /> // ici la page qui s'affichera en cas d'erreur (route non trouvée, etc)
   }
 
